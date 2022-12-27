2079686ffbfbc77feca51cd712adb9abfafcca77
MakedonskyVladimir <shurmanoff@mail.ru>
2022-12-27 07:10:25 +0300

    added components forder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import React from 'react';
+import Users from './components/Users/Users'
+import Success from './components/Success/Success'
+
+// список пользователей: http://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([])
+
+  // let response = fetch('http://reqres.in/api/users')
+
   return (
     <div>
       <h1>Список пользователей</h1>
+      <h2></h2>
     </div>
   );
 }
